ea4c4cb9575c3d40dab7dade8aed607c045c2dc7
Mauricio Paz <mauriciopazdev@gmail.com>
2023-11-03 14:38:50 -0300

    First commit

---
+++ app.js
@@ -0,0 +1,70 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+require('dotenv').config();
+const { getBCAuth, buildRedirectUrl, getBCVerify } = require('./bigcommerce');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/auth', async (req, res) => {
+    try {
+        const session = await getBCAuth(req.query);
+        const encodedContext = encodePayload(session);
+        global.session = session;
+        res.redirect(302, `/?context=${encodedContext}`);
+    } catch (error) {
+        const { message, response } = error;
+        res.status(response?.status || 500).json({ message });
+    }
+});
+
+app.get('/load', async (req, res) => {
+    try {
+        const session = await getBCVerify(req.query);
+        const encodedContext = encodePayload(session);
+        global.session = session;
+
+        res.redirect(302, buildRedirectUrl(session.url, encodedContext));
+    } catch (error) {
+        const { message, response } = error;
+        res.status(response?.status || 500).json({ message });
+    }
+});
+
+app.get('/logout', (req, res) => {
+    try {
+        global.session = null;
+        res.status(200).end();
+    } catch (error) {
+        const { message, response } = error;
+        res.status(response?.status || 500).json({ message });
+    }
+});
+
+app.get('/uninstall', async (req, res) => {
+    try {
+        global.session = null;
+        res.status(200).end();
+    } catch (error) {
+        const { message, response } = error;
+        res.status(response?.status || 500).json({ message });
+    }
+});
+
+app.get('/remove-user', async (req, res) => {
+    try {
+        global.session = null;
+        res.status(200).end();
+    } catch (error) {
+        const { message, response } = error;
+        res.status(response?.status || 500).json({ message });
+    }
+});
+
+app.get('/', (req, res) => {
+    res.send('home');
+});
+
+app.listen(4000, function(){
+    console.log("SERVER STARTED ON localhost:4000");
+});
---
+++ bigcommerce.js
@@ -0,0 +1,45 @@
+import * as jwt from 'jsonwebtoken';
+const BigCommerce = require('node-bigcommerce');
+require('dotenv').config();
+
+const { API_URL, AUTH_CALLBACK, CLIENT_ID, CLIENT_SECRET, JWT_KEY, LOGIN_URL } = process.env;
+
+export const bigcommerce = new BigCommerce({
+    logLevel: 'info',
+    clientId: CLIENT_ID,
+    secret: CLIENT_SECRET,
+    callback: AUTH_CALLBACK,
+    responseType: 'json',
+    headers: { 'Accept-Encoding': '*' },
+    apiVersion: 'v3',
+});
+
+const bigcommerceSigned = new BigCommerce({
+    secret: CLIENT_SECRET,
+    responseType: 'json',
+});
+
+export function getBCAuth(query) {
+    return bigcommerce.authorize(query);
+}
+
+export function getBCVerify({ signed_payload_jwt }) {
+    return bigcommerceSigned.verifyJWT(signed_payload_jwt);
+}
+
+export function encodePayload({ user, owner, ...session }) {
+    if (!session) {
+        return;
+    }
+    const contextString = session.context ? session.context : session.sub;
+    const context = contextString.split('/')[1] || '';
+
+    return jwt.sign({ context, user, owner }, JWT_KEY, { expiresIn: '24h' });
+}
+
+export const buildRedirectUrl = (url, encodedContext) => {
+    const [path, query = ''] = url.split('?');
+    const queryParams = new URLSearchParams(`context=${encodedContext}&${query}`);
+
+    return `${path}?${queryParams}`;
+}
